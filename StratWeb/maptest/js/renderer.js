d1f66ddf9ad4c46e00bee7e00cbed59ba0ece860
Steve <s.mattison@gmail.com>
2025-03-12 18:16:43 -0700

    Added server batch files, unit movement, unit selection > v0.235

--- StratWeb/maptest/js/renderer.js
+++ StratWeb/maptest/js/renderer.js
@@ -1,10 +1,9 @@
 // Log issues to console?
-const DEBUG=true;
-import{Unit}from "./unit.js"; //(id, point, x, y, type, speed, owner, graphic)
-
+	const DEBUG=true;
+	import{Unit}from "./unit.js"; //(id, point, x, y, type, speed, owner, graphic)
 
 // Make the paper scope global, by injecting it into window:
-paper.install(window);
+	paper.install(window);
 
 // Only executed our code once the DOM is ready.
 window.onload = function() {
@@ -14,10 +13,21 @@ window.onload = function() {
 	const button = document.getElementById("mode");
 	button.addEventListener("click", doButtonClick);
 	window.addEventListener("resize", doCanvasResize);
+	document.addEventListener("keydown", (event) => {
+		switch (event.key.toLowerCase()) {
+			case "s":doModeSelect();break;
+			case "m":doModeMove();break;
+			case "b":doModeBuild();break;
+		}
+	});
 
 //INIT
 	var paths = [], units = [];
 	var tool = new paper.Tool();
+	//Selection variables
+	const selectionRadius = 50;
+	let lastClickPoint = null;
+	let nearbyUnits = [];
 	let touch = { //mode, selected(0-none, 1-unit, 2-land), which
 		mode:0, //[0-build, 1-select, 2-move]
 		selected:0, //[0-unselected, 1-unit, 2-land]
@@ -28,6 +38,7 @@ window.onload = function() {
 		name:"user0",
 		xp:0
 	};
+
 //FUNCTIONALITY
 	//events
 	function doCanvasResize() {
@@ -35,6 +46,22 @@ window.onload = function() {
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
 		paper.view.viewSize = new paper.Size(window.innerWidth, window.innerHeight);
+	}
+	function doModeBuild(){
+		button.value="Mode: Build";		
+		touch.mode=0;
+	}
+	function doModeSelect(){
+		if(touch.mode!=1){
+			button.value="Mode: Select";		
+			touch.mode=1;
+		}
+		let point = {x:0,y:0};
+		toggleSelect(point);
+	}
+	function doModeMove(){
+		button.value="Mode: Move";		
+		touch.mode=2;
 	}
 	function doButtonClick(){
 		let buttonmode = doButtonInc();
@@ -53,15 +80,15 @@ window.onload = function() {
 		}
 	};
 	function doTouch(point){ //todo: Remove arbitrary selection test code
+console.log(touch.mode+", "+touch.selected+", "+touch.which);
+		if(DEBUG) console.log("TOUCHED: "+point);
 		switch(touch.mode){
-			case 2: //move unit // TELEPORT!!
+			case 2: //move unit
 				var id=touch.which;
 				if(typeof(units[id]) != typeof(undefined)){
 					if(touch.selected==1){
-						units[id].point.position = point; //move the 0th unit
-						units[id].x = point.x;
-						units[id].y = point.y;
-						if(DEBUG) console.log("Unit "+touch.which+" moved to "+point);
+						units[id].destination = point; //move the 0th unit
+						if(DEBUG) console.log("Unit "+touch.which+" moving to "+point);
 					}else{
 						if(DEBUG) console.log("Warning! Unit not selected!");						
 					}
@@ -70,13 +97,7 @@ window.onload = function() {
 				}
 			break;
 			case 1: //select object // TOGGLE!!
-				if(touch.selected==0 && touch.which==0){
-					touch.selected = 1;
-					pickUnit(0);
-				}else if(touch.selected == 1){
-					//cycle through units
-					pickUnit(touch.which+1); //select this one
-				}
+				toggleSelect(point);
 			break;
 			default: //build unit // ANYWHERE!!
 				let newguy = units.length||0;
@@ -114,7 +135,7 @@ window.onload = function() {
 	}
 	function buildUnit(id,pt){ //id, point
 		if(!unitIdExists(id,units)){
-			if(DEBUG) console.log("Building unit "+id+"...");
+			if(DEBUG) console.log("Building unit "+id+" at "+pt+"...");
 			//constructor(id, point, type, speed, owner, graphic)
 			var x=pt.x;
 			var y=pt.y;
@@ -128,12 +149,58 @@ window.onload = function() {
 		}
 	}
 	//selection
+	function toggleSelect(point){ //temporary, select toggling, for testing only!
+		console.log("Point: "+JSON.stringify(point));
+		console.log("X: "+point.x+", Y: "+point.y);
+
+		// Find all nearby units within selectionRadius, sorted by distance
+		let nearbyUnits = units
+			.map((unit, id) => {
+				if (!unit.point || !unit.point.position) return null;
+				const unitPos = unit.point.position;
+				return { id, dist: unitPos.subtract(point).length };
+			})
+			.filter(u => u && u.dist <= selectionRadius)
+			.sort((a, b) => a.dist - b.dist);
+
+		if (nearbyUnits.length === 0) {
+			// No units nearby, just select the terrain
+			pickLand(0);
+			return;
+		}
+
+		let nextIndex = 0;
+
+		if (touch.selected === 1) {
+			// Find the index of the currently selected unit
+			nextIndex = nearbyUnits.findIndex(u => u.id === touch.which) + 1;
+			if (nextIndex >= nearbyUnits.length) {
+				// If we've cycled through all units, select the terrain
+				pickLand(0);
+				return;
+			}
+		}
+
+		// Select the next unit in the cycle
+		pickUnit(nearbyUnits[nextIndex].id);
+	}
 	function unpickAllUnits(){ //not optimized, just unpick selected unit list!
 		units.forEach(unit=>{
 			unit.point.fillColor='black';
 		});
+		if (touch.selected === 1) { // Only clear selection if troop was selected
+			touch.selected = 0;
+			touch.which = 0;
+		}
 	}
 	function unpickAllLands(){ //unfinished
+		//deselect the square that exists
+		rect.selected = false;
+		rect.fillColor = "lightgreen";
+		if (touch.selected === 2) { // Only clear selection if land was selected
+			touch.selected = 0;
+			touch.which = 0;
+		}
 	}
 	function pickNone(){
 		unpickAllUnits();
@@ -142,28 +209,25 @@ window.onload = function() {
 		touch.which=0;
 		if(DEBUG) console.log("Select Nothing!");
 	}
-	function pickUnit(id){ //picks a single unit by id
-		unpickAllUnits();
-		touch.selected=1;
-		if(typeof(units[id]) != typeof(undefined)){
-			rect.selected = false;
-			rect.fillColor="lightgreen";
-			touch.which=id;
-			if(DEBUG) console.log("Selected unit: "+id);
-			units[id].point.fillColor='red';
-		}else{
-			touch.selected=0;
-			touch.which=0;
-			if(DEBUG) console.log("Terrain Selected!");
-			rect.selected = true;
-			rect.fillColor="#ccffcc";
+	function pickUnit(id) {
+		unpickAllUnits(); //deselect everything
+		touch.selected = id >= 0 ? 1 : 0; //units are selected so set flag
+		touch.which = id; //which unit is it
+
+		if (id >= 0) { //select a unit
+			unpickAllLands(); //deselect everything
+			units[id].point.fillColor = 'red'; // Highlight selected unit
+		} else {
+			pickLand(0);
 		}
 	}
 	function pickLand(id){ //picks a land by id
-		touch={selected:2,which:+id};
+		unpickAllUnits();
+		touch.selected=2;touch.which=id;
 		if(DEBUG) console.log("Select land: "+id);
+		rect.selected = true;
+		rect.fillColor = "#ccffcc"; // Highlight terrain
 	}
-
 //INPUTS
 	tool.onMouseDown = function(event) {
 		doTouch(event.point);
@@ -187,10 +251,9 @@ window.onload = function() {
 	});
 
 //TICK
-	/*function onFrame(){
-		units.forEach(unit=>{
-			unit.position += unit.delta;
-		});
-	}*/
+	view.onFrame = function(event) {
+		units.forEach(unit => unit.update(event.delta));
+	}
+
 	doCanvasResize(); // initial setup
 }
--- StratWeb/maptest/js/unit.js
+++ StratWeb/maptest/js/unit.js
@@ -1,22 +1,38 @@
 // units.js
 export class Unit {
-  constructor(id, point, type = 3, speed = 1, owner = 0, graphic = null) {
-    this.id = id;
-	this.point = point;
-    this.type = type; //1 land 2 sea 3 air
-    this.speed = speed;
-    this.owner = owner; //neutral
-	this.graphic = graphic;
-    this.x = point.x;
-    this.y = point.y;
-  }
+	constructor(id, point, type = 3, speed = 1, owner = 0, graphic = null, destination = null) {
+		this.id = id;
+		this.point = point;
+		this.type = type; //1 land 2 sea 3 air
+		this.speed = speed;
+		this.owner = owner; //neutral
+		this.graphic = graphic;
+		this.x = point.x;
+		this.y = point.y;
+		this.destination = destination;
+		this.position = null;
+	}
+	update(delta) {
+		if (!this.destination) return;
 
-  move(dx, dy) {
-    this.x += dx;
-    this.y += dy;
-  }
-  updateGraphic(){
-	  if(this.graphic){
-	  }
-  }
+		// Ensure position is a Paper.Point object
+		this.position = new Point(this.point.position.x, this.point.position.y);
+
+		let vector = this.destination.subtract(this.position);
+		let distance = vector.length;
+
+		if (distance < this.speed * delta * 20) {
+			this.position = this.destination.clone();
+			this.destination = null;
+			console.log("Point reached, stopping!");
+		} else {
+			let step = vector.normalize().multiply(this.speed * delta * 20);
+			this.position = this.position.add(step); // Update logical position
+		}
+
+		// Update the Paper.js circle's position
+		if (this.point) {
+			this.point.position = this.position.clone();
+		}
+	}
 }
